43714ce3f13f81b9ceb135cf4fd2a5cbb5140ff3
byds <15910529587@163.com>
2018-02-26 22:58:08 +0800

    test

---
+++ boot/src/main/java/com/ds/boot/Application.java
@@ -0,0 +1,21 @@
+package com.ds.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+@EnableAutoConfiguration
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+    @RequestMapping("/")
+	@ResponseBody
+	public String home() {
+	   return "Hello Spring Boot!";
+	}
+}
